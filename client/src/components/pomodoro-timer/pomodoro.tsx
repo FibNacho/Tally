39910902b418c2868541780ae59c2b1443e25bfc
santanajb03 <130883269+santanajb03@users.noreply.github.com>
2025-09-03 13:36:10 -0400

    Refactor of timer

--- client/src/components/pomodoro-timer/pomodoro.tsx
+++ client/src/components/pomodoro-timer/pomodoro.tsx
@@ -3,18 +3,7 @@ import { timeStamp, timeElapsed } from '../../util/time-stamp';
 import { type timeStampObj } from '../../util/time-stamp';
 
 function PomodoroTimer() {
-  const defaultTimeStamp: timeStampObj = {
-    hour: 0,
-    minute: 0,
-    second: 0,
-    milliSecond: 0,
-    month: 0,
-    day: 0,
-    year: 0,
-    timeNow: 0,
-  };
-
-  const [currTime, setCurrTIme] = useState(defaultTimeStamp);
+  const [currTime, setCurrTIme] = useState(timeStamp());
 
   return (
     <>
