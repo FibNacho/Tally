16a2fab94edef9bd82adfc9e7ab88134ba075a8c
santanajb03 <130883269+santanajb03@users.noreply.github.com>
2025-09-03 13:35:53 -0400

    Finished time stamp utility and have working demo

--- client/src/components/pomodoro-timer/pomodoro.tsx
+++ client/src/components/pomodoro-timer/pomodoro.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import { timeStamp, timeElapsed } from '../../util/time-stamp';
+import { type timeStampObj } from '../../util/time-stamp';
+
 function PomodoroTimer() {
+  const defaultTimeStamp: timeStampObj = {
+    hour: 0,
+    minute: 0,
+    second: 0,
+    milliSecond: 0,
+    month: 0,
+    day: 0,
+    year: 0,
+    timeNow: 0,
+  };
+
+  const [currTime, setCurrTIme] = useState(defaultTimeStamp);
+
   return (
     <>
-      <span>Place holder</span>
+      <button
+        onClick={() => {
+          setCurrTIme(timeStamp());
+        }}
+      >
+        <span>{` Hour:${currTime.hour} Minute:${currTime.minute} Second:${currTime.second}`}</span>
+      </button>
     </>
   );
 }
