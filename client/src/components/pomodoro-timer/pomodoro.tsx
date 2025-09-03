06ec8584fc521d9d7f9b494494cca41b8ba82c39
santanajb03 <130883269+santanajb03@users.noreply.github.com>
2025-09-03 18:20:38 -0400

    Scrapped the stupid time util and got a working timer completed with no styling

--- client/src/components/pomodoro-timer/pomodoro.tsx
+++ client/src/components/pomodoro-timer/pomodoro.tsx
@@ -1,18 +1,60 @@
-import { useState } from 'react';
-import { timeStamp, timeElapsed } from '../../util/time-stamp';
-import { type timeStampObj } from '../../util/time-stamp';
+import { useEffect, useRef, useState } from 'react';
+import { FaPlay, FaPause } from 'react-icons/fa';
+import { GrRefresh } from 'react-icons/gr';
 
 function PomodoroTimer() {
-  const [currTime, setCurrTIme] = useState(timeStamp());
+  const defaultTime = 1500000;
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(defaultTime);
+  const [startTime, setStartTime] = useState(0);
+  const intervalRef = useRef(0);
+
+  useEffect(() => {
+    if (isPlaying) {
+      const intervalId = setInterval(() => {
+        setTimeLeft(() => defaultTime - (Date.now() - startTime));
+      }, 1);
+      intervalRef.current = intervalId;
+    }
+  }, [isPlaying]);
+
+  function handleStart() {
+    setIsPlaying((s) => !s);
+    setStartTime(() => Date.now());
+  }
+
+  function handleStop() {
+    setIsPlaying((s) => !s);
+    clearInterval(intervalRef.current);
+  }
+
+  function handleRestart() {
+    clearInterval(intervalRef.current);
+    setIsPlaying(() => false);
+    setTimeLeft(defaultTime);
+  }
+
+  function convertMilliseconds(milliseconds: number) {
+    return {
+      seconds: Math.floor((milliseconds / 1000) % 60),
+      minutes: Math.floor(milliseconds / 1000 / 60),
+    };
+  }
+
+  const displayTime = convertMilliseconds(timeLeft);
 
   return (
     <>
-      <button
-        onClick={() => {
-          setCurrTIme(timeStamp());
-        }}
-      >
-        <span>{` Hour:${currTime.hour} Minute:${currTime.minute} Second:${currTime.second}`}</span>
+      <span>{`${
+        displayTime.minutes < 10 ? `0${displayTime.minutes}` : displayTime.minutes
+      }:${
+        displayTime.seconds < 10 ? `0${displayTime.seconds}` : displayTime.seconds
+      }`}</span>
+      <button onClick={isPlaying ? handleStop : handleStart}>
+        {isPlaying ? <FaPause /> : <FaPlay />}
+      </button>
+      <button onClick={handleRestart}>
+        <GrRefresh></GrRefresh>
       </button>
     </>
   );
